5721e95d4653759bfc750d7e3fb3383e8a86b63b
LevitD <diml71@mail.ru>
2023-08-16 18:45:20 +0300

    filter

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,37 +1,37 @@
 const express = require('express')
 const Product = require('../models/Product')
-const router = express.Router({mergeParams:true})
+const router = express.Router({ mergeParams: true })
 const config = require('config')
 const auth = require("../middleware/auth.middleware");
 const cors = require('cors');
 const mongoose = require("mongoose");
 
 router.route('/:productId')
-    .get(async (req,res)=>{
-    const {productId} = req.params
-    let list
-    try {
-        if (productId==="array"){
-            const newArra = JSON.parse(req.query.array)
-            // // В body передаем массив с id нужных товаров
-            list = await Product.find({_id: {$in: newArra}})
-        } else {
-            list = await Product.findOne({_id: productId})
-        }
-        res.status(200).send(list)
-    } catch   {
-        res.status(500).json({
-            message: '(productId)На сервере произошла ошибка, попробуйте позже...'
-        })
-    }
-})
-    .patch(auth,async (req, res)=>{
+    .get(async (req, res) => {
+        const { productId } = req.params
+        let list
         try {
-            const id =new mongoose.Types.ObjectId(req.body._id)
-            const updateProduct = await Product.findByIdAndUpdate(id,{
+            if (productId === "array") {
+                const newArra = JSON.parse(req.query.array)
+                // // В body передаем массив с id нужных товаров
+                list = await Product.find({ _id: { $in: newArra } })
+            } else {
+                list = await Product.findOne({ _id: productId })
+            }
+            res.status(200).send(list)
+        } catch {
+            res.status(500).json({
+                message: '(productId)На сервере произошла ошибка, попробуйте позже...'
+            })
+        }
+    })
+    .patch(auth, async (req, res) => {
+        try {
+            const id = new mongoose.Types.ObjectId(req.body._id)
+            const updateProduct = await Product.findByIdAndUpdate(id, {
                 ...req.body,
-                user_id:req.user._id
-            },{new:true})
+                user_id: req.user._id
+            }, { new: true })
             res.status(201).send(updateProduct)
         } catch (e) {
             console.log(e)
@@ -43,24 +43,25 @@ router.route('/:productId')
 
 router
     .route('/')
-    .get(cors(), async (req,res)=>{
-    let {skip, user_id} = req.query ? req.query : 0
+    .get(cors(), async (req, res) => {
+
+        let { skip, user_id, filter } = req.query ? req.query : 0
         skip = !skip ? 0 : skip
         // console.log(req.query,skip)
-    try {
-        const list = await Product.find(user_id ? {user_id:user_id } :{}).sort( { createdAt : -1 }).skip( skip ).limit( config.get('productGetLimit') )
-        res.status(200).send(list)
-    } catch   {
-        res.status(500).json({
-            message: '(/)На сервере произошла ошибка, попробуйте позже...'
-        })
-    }
+        try {
+            const list = await Product.find(user_id ? { user_id: user_id } : filter ? JSON.parse(filter) : {}).sort({ createdAt: -1 }).skip(skip).limit(config.get('productGetLimit'))
+            res.status(200).send(list)
+        } catch {
+            res.status(500).json({
+                message: '(/)На сервере произошла ошибка, попробуйте позже...'
+            })
+        }
     })
-    .post(auth,async (req, res)=>{
+    .post(auth, async (req, res) => {
         try {
             const newProduct = await Product.create({
                 ...req.body,
-                user_id:req.user._id
+                user_id: req.user._id
             })
             res.status(201).send(newProduct)
         } catch (e) {
@@ -72,4 +73,4 @@ router
     })
 
 
-module.exports=router
+module.exports = router
